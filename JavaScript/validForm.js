194dc0a78f8f7a7e25e29a1245bbe599332b9a36
rafael lopes <rafalopes900@hotmail.com>
2023-12-08 22:51:54 -0300

    Atualizando

--- JavaScript/validForm.js
+++ JavaScript/validForm.js
@@ -1,17 +1,33 @@
-(function() {
-    'use strict'
+var form = document.querySelector('form');
 
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+var nomeBandaArtista = document.getElementById('bandaNome');
+var generoMusical = document.getElementById('generoMusical');
+var numIntegrantes = document.getElementById('numIntegrantes');
+var biografia = document.getElementById('validationTextarea');
+var siteBanda = document.getElementById('basic-url');
+
+var regexLetras = /^[a-zA-Z\s]+$/;
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    validarForm();
+});
+
+nomeBandaArtista.addEventListener("blur", () => {
+    validarNome();
+})
+
+function validarNome() {
+    if (!regexLetras.test(nomeBandaArtista.value)) {
+        nomeBandaArtista.classList.add('is-invalid');
+    } else {
+        nomeBandaArtista.classList.add('is-valid');
+    }
+}
+
+function validarForm() {
+    validarNome();
+
+    // Adiciona a classe was-validated ao formulário
+   
+}
